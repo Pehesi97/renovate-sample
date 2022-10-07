737bc735f730894b5147559551e331a391d81020
Pehesi97 <pedro.silva@wisenet.inf.br>
2022-10-07 00:49:49 -0300

    using renovate github action

--- renovate-config.js
+++ renovate-config.js
@@ -1,10 +1,10 @@
 module.exports = {
     endpoint: 'https://api.github.com/',
-    token: '**gitlab_token**',
     platform: 'github',
     onboardingConfig: {
       extends: ['config:base'],
     },
     repositories: ['pehesi97/renovate-sample'],
+    enabledManagers: ['terraform']
   };
   
